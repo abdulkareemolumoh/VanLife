da88447888af505412a7ce5d08ef65540976e1c3
Abdulkareem Olumoh <abdulkareemolumoh@protonmail.com>
2023-09-19 17:23:26 +0100

    API fixed

--- VanLife/pages/Vans/VanDetails.jsx
+++ VanLife/pages/Vans/VanDetails.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams, useLoaderData } from "react-router-dom";
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
 import { getVans } from "../../api";
 
 export function loader({ params }) {
@@ -7,40 +7,26 @@ export function loader({ params }) {
 }
 
 export default function VanDetails() {
-  const { id } = useParams();
-  const [van, setVan] = useState(null);
-  const data = useLoaderData();
-  
-  console.log(data);
+  const van = useLoaderData();
 
-  useEffect(() => {
-    fetch(`/api/vans/${id}`)
-      .then((res) => res.json())
-      .then((data) => setVan(data.vans));
-  }, [id]);
-
-  // console.log(van);
   return (
     <div className="container-fluid card">
       <Link to=".." relative="path">
         &larr; Back to all vans
       </Link>
-      {van ? (
-        <div>
-          <img src={van.imageUrl} className="card-img-top" />
-          <div className="card-body text-start ">
-            <h2 className="card-text p-2">{van.name}</h2>
-            <h2 className="card-text p-2">{van.type}</h2>
-            <h2 className="card-text p-2">{van.price}/day</h2>
-            <h2 className="card-text p-2">{van.description}</h2>
-            <div className="d-grid gap-2 col-11 mx-auto pt-4 ">
-              <button className="btn  btn-warning py-2">Rent this van</button>
-            </div>
+
+      <div>
+        <img src={van.imageUrl} className="card-img-top" />
+        <div className="card-body text-start ">
+          <h2 className="card-text p-2">{van.name}</h2>
+          <h2 className="card-text p-2">{van.type}</h2>
+          <h2 className="card-text p-2">{van.price}/day</h2>
+          <h2 className="card-text p-2">{van.description}</h2>
+          <div className="d-grid gap-2 col-11 mx-auto pt-4 ">
+            <button className="btn  btn-warning py-2">Rent this van</button>
           </div>
         </div>
-      ) : (
-        <h1>Loading...</h1>
-      )}
+      </div>
     </div>
   );
 }
